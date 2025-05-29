8486d708d007ed243cd0b56bd670e4aef4576b4c
khuang26 <148280685+khuang26@users.noreply.github.com>
2025-05-29 10:07:03 -0400

    rewrote as object-oriented

--- Data Processing.py
+++ Data Processing.py
@@ -2,62 +2,63 @@ import pandas as pd
 import numpy as np
 
 
-# HELPER FUNCTIONS
 
-# Get interest rate for a given pair
-def getRate(row): 
-	S = row['u_trade_px']
-	K = row['strike_price']
-	C = row['trade_price_call']
-	P = row['trade_price_put']
-	T = row['year_to_expiration']
+class OptionsData: 
+	def __init__(self, fileName):
+		self.fileName = fileName
+		self.df = pd.read_csv(fileName)
+		self.__calls = self.df[self.df['is_call'] == 1]
+		self.__puts = self.df[self.df['is_call'] == 0]
+		self.__pairs = None
+		self.__avgRate = None
+		self.__rateStDev = None
 
-	if T <= 0 or K <= 0 or (S - C + P) <= 0:
-		return np.nan
-
-	r = - (np.log((S - C + P) / K)) / T
-	return r
-
-
-# Print valid put-call pairs
-def printPairs(): 
-	print(pairs.head())
+	def computeRFRate(self):
+		self.__findPairs()
+		self.__computeSeriesRates()
+		self.setAvgRate(self.__pairs['rate'].mean())
+		self.__rateStDev = self.__pairs['rate'].std()
+		print(f"The average risk-free rate across all valid put-call pairs is: {self.__avgRate} and the standard deviation is {self.__rateStDev}")
+		self.__writePairs()
 
 
-# Get avg. interest rate across all corresponding put-call pairs
-def getAvgRate(): 
-	return pairs['rate'].mean()
+	def __findPairs(self): 
+		self.__pairs = pd.merge(
+			self.__calls[['strike_price', 'year_to_expiration', 'u_trade_px', 'trade_price', 'trade_qty']],
+			self.__puts[['strike_price', 'year_to_expiration', 'u_trade_px', 'trade_price', 'trade_qty']],
+			on=['strike_price', 'year_to_expiration', 'u_trade_px'], # Columns that need to match
+			suffixes=('_call', '_put')
+		)
+	
+	def __printPairs(self): 
+		print(self.__pairs.head())
+	
+	def __writePairs(self):
+		self.__pairs.to_csv("Pairs.csv", index=False)
 
-# Write to CSV
-def writePairs(): 
-	pairs.to_csv("Pairs.csv", index=False)
+	def __getRate(self, row): 
+		S = row['u_trade_px']
+		K = row['strike_price']
+		C = row['trade_price_call']
+		P = row['trade_price_put']
+		T = row['year_to_expiration']
 
+		if T <= 0 or K <= 0 or (S - C + P) <= 0:
+			return np.nan
 
+		r = - (np.log((S - C + P) / K)) / T
+		return r
+		
+	def __computeSeriesRates(self): 
+		self.__pairs['rate'] = self.__pairs.apply(self.__getRate, axis = 1) # new column "rate" created in DataFrame; axis = 1 --> checks each row
 
-# MAIN CODE
+	def getAvgRate(self):
+		return self.__avgRate
+	
+	def setAvgRate(self, rate): 
+		oldRate = self.__avgRate
+		self.__avgRate = rate
+		return oldRate
 
-df = pd.read_csv('Options Data - data.csv')
-
-calls = df[df['is_call'] == 1]
-puts = df[df['is_call'] == 0]
-
-# DataFrame with matching put-call pairs
-pairs = pd.merge(
-    calls[['strike_price', 'year_to_expiration', 'u_trade_px', 'trade_price', 'trade_qty']],
-    puts[['strike_price', 'year_to_expiration', 'u_trade_px', 'trade_price', 'trade_qty']],
-    on=['strike_price', 'year_to_expiration', 'u_trade_px'], # Columns that need to match
-    suffixes=('_call', '_put')
-)
-
-pairs['rate'] = pairs.apply(getRate, axis = 1) # new column "rate" created in DataFrame; axis = 1 --> checks each row
-
-for i in pairs.index:
-	if pairs.loc[i, 'rate'] < 0:
-		pairs.drop(i, inplace = True)
-
-
-avgRate = getAvgRate()
-rateStDev = pairs['rate'].std()
-print(f"The average risk-free rate across all valid put-call pairs is: {avgRate} and the standard deviation is {rateStDev}")
-
-writePairs()
+vale = OptionsData("Options Data - data.csv")
+vale.computeRFRate()
