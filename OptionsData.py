299ff00819caae1305a8bc70e093849ca8bd23ef
khuang26 <148280685+khuang26@users.noreply.github.com>
2025-06-03 15:46:50 -0400

    added methods to calculate greeks

--- OptionsData.py
+++ OptionsData.py
@@ -13,9 +13,10 @@ class OptionsData:
 		self.__pairs = None
 		self.__avgRate = None
 		self.__rateStDev = None
+		self.__interest_rates = None
 
 
-	def __findPairs(self, minTime = 0, maxTime = 36000, maxTimeGap = 36000): 
+	def __findPairs(self, minTime = 0, maxTime = 28800, maxTimeGap = 28800): 
 		self.__pairs = pd.merge(
 			self.__calls[['seconds','symbol', 'strike_price', 'year_to_expiration', 'u_trade_px', 'trade_price', 'trade_qty']],
 			self.__puts[['seconds', 'symbol', 'strike_price', 'year_to_expiration', 'u_trade_px', 'trade_price', 'trade_qty']],
@@ -46,32 +47,33 @@ class OptionsData:
 
 		self.__pairs['trade_qty'] = self.__pairs['trade_qty_call'] + self.__pairs['trade_qty_put']
 		
-		self.__pairs['seconds'] = (self.__pairs['seconds_call'] + self.__pairs['seconds_put'])/2
+		self.__pairs['seconds'] = (self.__pairs['seconds_call'] + self.__pairs['seconds_put'])/2 # Should change this to be max()
 
-	def computeRFRate(self, minTime = 0, maxTime = 36000, maxTimeGap = 600):
+	def computeRFRate(self, minTime = 0, maxTime = 28800, maxTimeGap = 600):
 		self.__findPairs(minTime, maxTime, maxTimeGap)
 		self.__computeSeriesRates()
 		
 		weighted_sum = np.dot(self.__pairs['rate'], self.__pairs['trade_qty'])
 		total_qty = self.__pairs['trade_qty'].sum()
 		weighted_avg_rate = weighted_sum / total_qty
-		self.setAvgRate(weighted_avg_rate)
+		
 
 		weighted_var = ((self.__pairs['rate'] - weighted_avg_rate)**2 * self.__pairs['trade_qty']).sum() / total_qty
-		self.__rateStDev = np.sqrt(weighted_var)
+		weighted_StDev = np.sqrt(weighted_var)
 
-		print(f'The average risk-free rate across all valid put-call pairs occurring between {minTime} and {maxTime} seconds with a max time difference of {maxTimeGap} seconds is {self.__avgRate}, and the standard deviation is {self.__rateStDev}.')
+		if (minTime == 0) and (maxTime == 28800) and (maxTimeGap == 600): 
+			self.setAvgRate(weighted_avg_rate)
+			self.__rateStDev = weighted_StDev
+
+		print(f'The average risk-free rate across all valid put-call pairs occurring between {minTime} and {maxTime} seconds with a max time difference of {maxTimeGap} seconds is {weighted_avg_rate}, and the standard deviation is {weighted_StDev}.')
 		self.__writePairs()
 
-		return self.__avgRate
-	
-	def __printPairs(self): 
-		print(self.__pairs.head())
+		return weighted_avg_rate
 	
 	def __writePairs(self):
 		self.__pairs.to_csv('Pairs.csv', index=False)
 
-	def __checkRowValidity(self, row, minTime = 0, maxTime = 36000, maxTimeGap = 600):
+	def __checkRowValidity(self, row, minTime = 0, maxTime = 28800, maxTimeGap = 600):
 		return not (
 			(abs(row['seconds_call'] - row['seconds_put']) > maxTimeGap) 
 			or (row['seconds_call'] > maxTime) 
@@ -98,6 +100,23 @@ class OptionsData:
 		self.__pairs['rate'] = self.__pairs.apply(
 			self.__getRate, 
 			axis = 1) 
+		
+	def computeGreeks(self): 
+		self.df['delta'] = self.df.apply(
+			self.__computeDelta, 
+			axis = 1)
+		self.df['gamma'] = self.df.apply(
+			self.__computeGamma, 
+			axis = 1)
+		self.df['vega'] = self.df.apply(
+			self.__computeVega, 
+			axis = 1)
+		self.df['theta'] = self.df.apply(
+			self.__computeTheta, 
+			axis = 1)
+		self.df['rho'] = self.df.apply(
+			self.__computeRho, 
+			axis = 1)
 
 	def getAvgRate(self):
 		return self.__avgRate
@@ -123,27 +142,28 @@ class OptionsData:
 		self.df.['BSP']
 		for idx, row in self.df.iterrows(): """
 			
-	
+	def __createInterestRateTable(self): 
+		self.__interest_rates = []
+		for i in range(0, 32): 
+			self.__interest_rates.append(self.computeRFRate(900*i, 900*(i+1), 600))
+
 	def computeImpliedVolatilities(self, maxIterations = 100, tolerance = 0.005): 
 		self.df['implied_vol'] = np.nan
-		
-		# Creating a lookup table since it's probably not feasible to compute the RF rate for each data point
-		interest_rates = []
-		for i in range(0, 32): 
-			interest_rates.append(self.computeRFRate(900*i, 900*(i+1), 600))
-		
+
+		self.__createInterestRateTable()
+
 		for idx, row in self.df.iterrows():
 			S = row['u_trade_px']
 			K = row['strike_price']
 			T = row['year_to_expiration']
 			t = row['seconds']
 			if t>=900: 
-				r = interest_rates[math.floor(t/900) - 1]
+				r = self.__interest_rates[math.floor(t/900) - 1]
 			else: 
 				continue
 			market_price = row['trade_price']
 
-			is_call = row['is_call'] == 1
+			is_call = (row['is_call'] == 1)
 
 			iv = 0.3
 
@@ -164,6 +184,113 @@ class OptionsData:
 		
 		print("IV calculations done!")
 
+    # Greeks
+	def __computeDelta(self ,row): 
+		S = row['u_trade_px']
+		K = row['strike_price']
+		T = row['year_to_expiration']
+		sigma = row['implied_vol']
+		t = row['seconds']
+		is_call = (row['is_call'] == 1)
+		
+		if t>=900: 
+			r = self.__interest_rates[math.floor(t/900) - 1]
+		else: 
+			return np.nan
+
+		d1 = (np.log(S / K) + (r + sigma**2 / 2) * T) / (sigma * np.sqrt(T))
+		d2 = d1 - sigma * np.sqrt(T)
+
+		if is_call: 
+			delta = norm.cdf(d1)
+		else: 
+			delta = norm.cdf(d1) - 1
+		
+		return delta
+
+	def __computeGamma(self, row): 
+		S = row['u_trade_px']
+		K = row['strike_price']
+		T = row['year_to_expiration']
+		sigma = row['implied_vol']
+		t = row['seconds']
+		is_call = (row['is_call'] == 1)
+
+		if t>=900: 
+			r = self.__interest_rates[math.floor(t/900) - 1]
+		else: 
+			return np.nan
+		
+		d1 = (np.log(S / K) + (r + sigma**2 / 2) * T) / (sigma * np.sqrt(T))
+		d2 = d1 - sigma * np.sqrt(T)
+		
+		gamma = norm.pdf(d1) * 1/(S * sigma * np.sqrt(T))
+		return gamma
+	
+	def __computeVega(self, row): 
+		S = row['u_trade_px']
+		K = row['strike_price']
+		T = row['year_to_expiration']
+		sigma = row['implied_vol']
+		t = row['seconds']
+		is_call = (row['is_call'] == 1)
+
+		if t>=900: 
+			r = self.__interest_rates[math.floor(t/900) - 1]
+		else: 
+			return np.nan
+		
+		d1 = (np.log(S / K) + (r + sigma**2 / 2) * T) / (sigma * np.sqrt(T))
+		d2 = d1 - sigma * np.sqrt(T)
+		
+		vega = S * norm.pdf(d1) * np.sqrt(T)
+		return vega
+	
+	def __computeTheta(self, row): 
+		S = row['u_trade_px']
+		K = row['strike_price']
+		T = row['year_to_expiration']
+		sigma = row['implied_vol']
+		t = row['seconds']
+		is_call = (row['is_call'] == 1)
+
+		if t>=900: 
+			r = self.__interest_rates[math.floor(t/900) - 1]
+		else: 
+			return np.nan
+		
+		d1 = (np.log(S / K) + (r + sigma**2 / 2) * T) / (sigma * np.sqrt(T))
+		d2 = d1 - sigma * np.sqrt(T)
+		
+		if is_call: 
+			theta = S * norm.pdf(sigma/(2*np.sqrt(T))) * sigma / (2*np.sqrt(T)) + r * K * np.exp(-r * T) * norm.cdf(d2)
+		else: 
+			theta = S * norm.pdf(sigma/(2*np.sqrt(T))) * sigma / (2*np.sqrt(T)) + r * K * np.exp(-r * T) * norm.cdf(-d2)
+		return theta
+		
+	def __computeRho(self, row): 
+		S = row['u_trade_px']
+		K = row['strike_price']
+		T = row['year_to_expiration']
+		sigma = row['implied_vol']
+		t = row['seconds']
+		is_call = (row['is_call'] == 1)
+
+		if t>=900: 
+			r = self.__interest_rates[math.floor(t/900) - 1]
+		else: 
+			return np.nan
+		
+		d1 = (np.log(S / K) + (r + sigma**2 / 2) * T) / (sigma * np.sqrt(T))
+		d2 = d1 - sigma * np.sqrt(T)
+		
+		if is_call: 
+			rho = T * K * np.exp(-r*T) * norm.cdf(d2)
+		else: 
+			rho = -T * K * np.exp(-r*T) * norm.cdf(-d2)
+		return rho
+
+
 	def writeDF(self): 
 		self.df.to_csv('OptionsData.csv', index=False)
 
@@ -177,4 +304,5 @@ vale.computeRFRate(0, 28800, 28800)
 vale.computeRFRate(0, 28800, 600)"""
 
 vale.computeImpliedVolatilities()
+vale.computeGreeks()
 vale.writeDF()
